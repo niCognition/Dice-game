194b25fe11461854b0028f2ca9729b77ebdc5dae
Alexander <2683344+terminalnode@users.noreply.github.com>
2019-09-15 22:22:29 +0200

    implementerade en funktion som frågar efter heltal på ett säkrare sätt

--- Tärningspelv3/src/com/exercise/tarningv3/TarningV3.java
+++ Tärningspelv3/src/com/exercise/tarningv3/TarningV3.java
@@ -6,44 +6,39 @@ public class TarningV3 {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
-        int antalKast;
+        int antalKast, antalSpelare;
         int antalSidor = 6;
-        int antalSpelare;
         int table[][] = new int[100][100];
         int datorSlag[] = new int[100];
         int spelarPoang[] = new int[100];
         int highScore[] = new int[100];
-        String spelaIgen;
-        String datorVal;
+        String spelaIgen, datorVal;
 
-        System.out.println(" _______ _   _  _____  _   _ _____ _   _  _____  _____ _____  ______ _      _ ");
-        System.out.println("|__   __(_)_(_)|  __ \\| \\ | |_   _| \\ | |/ ____|/ ____|  __ \\|  ____| |    | |");
-        System.out.println("   | |    / \\  | |__) |  \\| | | | |  \\| | |  __| (___ | |__) | |__  | |    | |");
-        System.out.println("   | |   / _ \\ |  _  /| . ` | | | | . ` | | |_ |\\___ \\|  ___/|  __| | |    | |");
-        System.out.println("   | |  / ___ \\| | \\ \\| |\\  |_| |_| |\\  | |__| |____) | |    | |____| |____|_|");
-        System.out.println("   |_| /_/   \\_\\_|  \\_\\_| \\_|_____|_| \\_|\\_____|_____/|_|    |______|______(_)");
-        System.out.println("             Högst poäng efter att tärningar slagits vinner!");
-        System.out.println("");
+        System.out.println(
+                " _______ _   _  _____  _   _ _____ _   _  _____  _____ _____  ______ _      _ \n" +
+                "|__   __(_)_(_)|  __ \\| \\ | |_   _| \\ | |/ ____|/ ____|  __ \\|  ____| |    | |\n" +
+                "   | |    / \\  | |__) |  \\| | | | |  \\| | |  __| (___ | |__) | |__  | |    | |\n" +
+                "   | |   / _ \\ |  _  /| . ` | | | | . ` | | |_ |\\___ \\|  ___/|  __| | |    | |\n" +
+                "   | |  / ___ \\| | \\ \\| |\\  |_| |_| |\\  | |__| |____) | |    | |____| |____|_|\n" +
+                "   |_| /_/   \\_\\_|  \\_\\_| \\_|_____|_| \\_|\\_____|_____/|_|    |______|______(_)\n" +
+                "             Högst poäng efter att tärningar slagits vinner!\n");
 
         do {
             System.out.println("Hur många spelare är ni?");
-            antalSpelare = scan.nextInt();
-            scan.nextLine();
+            antalSpelare = queryNum(scan);
 
             System.out.println("Vill ni lägga till en dator? (y/n)");
             datorVal = scan.nextLine();
 
             System.out.println("Välj antal kast.");
-            antalKast = scan.nextInt();
-            scan.nextLine();
+            antalKast = queryNum(scan);
 
             if (datorVal.equalsIgnoreCase("y")){
                 for (int i = 1; i <= antalKast; i++){
                     datorSlag[i] = kast(antalSidor);
                     System.out.println("Dator kast " + i + " Poäng " + datorSlag[i]);
                 }
-
-            }else;
+            }
 
             System.out.println(" ");
 
@@ -64,6 +59,7 @@ public class TarningV3 {
             for (int i = 1; i <= antalKast; i++){
                 datorPoang += datorSlag[i];
             }
+
             System.out.println("Datorns totalpoäng: " + datorPoang);
 
             for (int n = 1; n <= antalSpelare; n++){
@@ -72,15 +68,17 @@ public class TarningV3 {
                 }
                 System.out.println("Totalpoäng spelare " + n + ": " + spelarPoang[n]);
             }
+
             int max = spelarPoang[0];
             for (int i = 1; i < spelarPoang.length; i++){
                 if (spelarPoang[i] > max){
                     max = spelarPoang[i];
                 }
             }
+
             if (datorPoang > max){
                 System.out.println("\nVinnaren är datorn med: " + datorPoang);
-            }else {
+            } else {
                 for (int i = 0; i <= antalSpelare; i++) {
                     if (spelarPoang[i] == max) {
                         System.out.println("\nVinnaren är spelare " + i + " med: " + max + " poäng!");
@@ -105,11 +103,41 @@ public class TarningV3 {
                 break;
             }
 
-        }while(!spelaIgen.equalsIgnoreCase("nej"));
+        } while(!spelaIgen.equalsIgnoreCase("nej"));
     }
 
     public static int kast(int inAntalsidor){
         int utKast = (int)(1 + inAntalsidor * Math.random());
         return utKast;
+    }
+
+    // Fråga användaren efter ett heltal tills ett rimligt heltal har angivits.
+    public static int queryNum(Scanner scanner) {
+        /* Initalisera returnValue till null så vi kan försöka få ett
+           värde till den while (returnValue == null) */
+        Integer returnValue = null;
+
+        while (returnValue == null) {
+            // Det här är vår prompt.
+            System.out.print("> ");
+
+            /* try {} försöker utföra någonting, om ett fel av typen Exception uppstår så
+               fångar catch (Exception e) det och försöker fixa situationen. Exception är en
+               klass som alla andra Exceptions ärver från och därmed kommer den fånga allt. */
+            try {
+                returnValue = Integer.valueOf(scanner.nextLine());
+
+                // Om vi kommit hit så vet vi att vi fått ett tal, men inte vad för tal.
+                if (returnValue <= 0) {
+                    returnValue = null; // Resetta värdet så att loopen kommer köras igen.
+                    System.out.println("Talet måste vara högre än noll. Försök igen.");
+                }
+
+            } catch (Exception e) {
+                System.out.println("Det där är inte ett heltal! Försök igen.");
+            }
+        }
+
+        return returnValue; // returnValue är en Integer, men konverteras automatiskt till int när det returneras.
     }
 }
